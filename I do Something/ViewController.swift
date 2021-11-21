47c74c83764e5316869017daa9d801a0476088fe
jakecyg <43617272+jakecyg@users.noreply.github.com>
2021-11-20 19:23:24 -0600

    Initial Commit

---
+++ I do Something/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I do Something
+//
+//  Created by Jake Choi on 2021-11-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
